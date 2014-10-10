972d60a82f441b4c7b2fc57f6fbaeaaa80bade8d
James Malone <jamalone at gmail.com>
2014-10-09 18:47:31 -0700

    Add script to rip Pronto bike share locations

---
+++ pronto_location_grab.py
@@ -0,0 +1,31 @@
+'''
+This script gets all of the locations for the Seattle Pronto bike share
+and dumps them out in delimited format.
+
+v1/2014.10.09
+'''
+
+# Metadata
+__author__ = 'James Malone'
+
+# Imports
+import json, requests
+
+# STATIC VARIABLES
+PRONTO_LOCATION_JSON_URL = 'http://secure.prontocycleshare.com/data2/stations.json'
+DELIMITER = '\t'
+
+# Get the data
+http_response = requests.get(url=PRONTO_LOCATION_JSON_URL)
+json_location_data = json.loads(http_response.text)
+
+# Output the data
+if json_location_data and 'stations' in json_location_data and json_location_data['stations'][0]:
+    # Print the headers
+    header = json_location_data['stations'][0].keys()
+    print(DELIMITER.join(map(str, header)))
+
+    # Print the station data
+    for station in json_location_data['stations']:
+        station_data = station.values()
+        print(DELIMITER.join(map(str, station_data)))
