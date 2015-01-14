6af4c50e6e3ff0b389efa02dec847636474f1291
James Malone <jamalone at gmail.com>
2015-01-14 09:55:16 -0800

    Grab Walkscore Data
    Script to grab walkscore-related data for a given list of cities.

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ RandomPython
 Random bits and pieces of python to make life more interesting.
 
 # List of scripts
-* airnow_history_grabber.py - Grabs historical AirNow air quality data
+* `airnow_history_grabber.py` - Grabs historical AirNow air quality data
+* `grab_walkscore_data.py` - Grabs score data from the Walkscore page for a given CSV list of cities (walk, bike, and transit scores)
---
+++ grab_walkscore_data.py
@@ -0,0 +1,87 @@
+"""
+This scripy scrapes data from the Walkscore website. This may be useful since their API has
+limits and getting a key can be annoyingly difficult. Please use this responsibily.
+
+This script consumes an input file in CSV format with the following format:
+
+    STATE, City
+
+The STATE code must be the two-letter US code, such as:
+
+    WA, Seattle
+
+@author - James Malone
+"""
+
+# Imports for this script
+import argparse
+import json
+import re
+import requests
+import sys
+import time
+
+# Constant variables
+BASE_URL = 'https://www.walkscore.com/%s/%s'
+NULL_SCORE_VALUE = 0
+REGEX_FILTERS = {
+    'walkscore': '\/\/pp.walk.sc\/badge\/walk\/score\/(\d+)\.svg',
+    'transitscore': '\/\/pp.walk.sc\/badge\/transit\/score\/(\d+)\.svg',
+    'bikescore': '\/\/pp.walk.sc\/badge\/bike\/score\/(\d+)\.svg'
+}
+SLEEP_INCREMENT_SECONDS = 1
+
+
+"""
+Parse the score data on the walkscore page.
+"""
+def parse_score_data(content):
+    return_data = [0,0,0]
+    score_hash = dict()
+    for score_type in REGEX_FILTERS.keys():
+        score_data = re.search(REGEX_FILTERS[score_type], content)
+        if score_data is not None:
+            score_hash[score_type] = score_data.group(1)
+        else:
+            score_hash[score_type] = NULL_SCORE_VALUE
+    return score_hash
+
+
+"""
+Return a well-formatted Walkscore URL
+"""
+def return_walkscore_url(state, city):
+    return BASE_URL % (state, city.replace(' ', '_'))
+
+
+"""
+Main method - executes work for this script.
+"""
+def main():
+    # Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-in", "--city_data", help="CSV city data")
+    parser.add_argument("-out", "--output_file", help="Output file")
+    args = parser.parse_args()
+    
+    with open(args.city_data) as city_file:
+        with open(args.output_file, "w") as out_file:
+            for line in city_file:
+                
+                # Parse the line and get URL
+                parts = line.strip().split(',')
+                print("Getting data for %s/%s" % (parts[0], parts[1]))
+                city_url = return_walkscore_url(parts[0], parts[1])
+                
+                # Fetch the data; write to file
+                r = requests.get(city_url)
+                page_data = str(r.content)
+                score_data = parse_score_data(page_data)
+                out_file.write("%s,%s,%s,%s,%s\n" % (parts[0], parts[1], score_data['walkscore'], score_data['transitscore'], score_data['bikescore']))
+                out_file.flush()
+                
+                # Sleep to be nice
+                time.sleep(SLEEP_INCREMENT_SECONDS)
+
+if __name__ == "__main__":
+    main()
